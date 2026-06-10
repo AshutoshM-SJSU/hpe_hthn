6601dbca3e2d81cf2137c35f52f5a90c325fa2ce
AshutoshM-SJSU <151249388+AshutoshM-SJSU@users.noreply.github.com>
2026-06-10 21:31:59 +0000

    add initial stor build

--- build/stor.c
+++ build/stor.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void win(void) {
+    printf("Arbitrary access achieved!\n");
+}
+
+static int invalid(void) {
+    printf("invalid\n");
+    return 255;
+}
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    return invalid();
+}
